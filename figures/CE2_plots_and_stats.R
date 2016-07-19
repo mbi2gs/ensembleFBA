60ef3da7db822ea800f256d99c597acec78dd148
mbi2gs <mb3ad@virginia.edu>
2016-07-19 11:31:40 -0400

    Solution time fold change
    Added new plot

--- figures/CE2_plots_and_stats.R
+++ figures/CE2_plots_and_stats.R
@@ -8,35 +8,44 @@ library(ggplot2)
 #-----------------------------------------------------------------
 # Read in data and calulate empirical 95% confidence intervals
 #-----------------------------------------------------------------
-seq_gms = c()
-seq_jacc = c()
+gms = c()
+jacc = c()
+times = c()
 for (i in c(2,5,10,15,20,25,30))
 {
   file_name = paste0("..\\data\\CE2_globalVsequential_",i,".tsv")
   seqData = read.table(file_name, row.names=NULL, sep = "\t", header=F)
   
-  # Calculate the difference in model size (parsimony of the solution)
+  # Calculate the difference in model size (parsimony of the solution) and solution time
   seqData$glob_minus_sequential = seqData[,4] - seqData[,3]
+  seqData$time_glob_div_seq = seqData[,6] / seqData[,5]
   
   # Estimate a 95% confidence interval assuming a normal distribution
   mean_gms = mean(seqData$glob_minus_sequential)
   quant_5_95 = quantile(seqData$glob_minus_sequential,c(0.025,0.975))
   tmp_gms = c(i,mean_gms,quant_5_95[1],quant_5_95[2])
-  seq_gms = rbind(seq_gms,tmp_gms)
+  gms = rbind(gms,tmp_gms)
   
   mean_jacc = mean(seqData[,1])
   quant_5_95 = quantile(seqData[,1],c(0.025,0.975))
   tmp_jacc = c(i,mean_jacc,quant_5_95[1],quant_5_95[2])
-  seq_jacc = rbind(seq_jacc,tmp_jacc)
+  jacc = rbind(jacc,tmp_jacc)
+  
+  mean_timeFold = mean(seqData$time_glob_div_seq)
+  quant_5_95 = quantile(seqData$time_glob_div_seq,c(0.025,0.975))
+  tmp_timeFold = c(i,mean_timeFold,quant_5_95[1],quant_5_95[2])
+  times = rbind(times,tmp_timeFold)
 }
-colnames(seq_gms) = c("Num.Growth.Conditions","Global.Minus.Seq","lower","upper")
-colnames(seq_jacc) = c("Num.Growth.Conditions","Ave.Jaccard.Sim","lower","upper")
-
-gms_df = data.frame(seq_gms)
-jacc_df = data.frame(seq_jacc)
+colnames(gms) = c("Num.Growth.Conditions","Global.Minus.Seq","lower","upper")
+colnames(jacc) = c("Num.Growth.Conditions","Ave.Jaccard.Sim","lower","upper")
+colnames(times) = c("Num.Growth.Conditions","Sol.Time.Fold.Change","lower","upper")
+  
+gms_df = data.frame(gms)
+jacc_df = data.frame(jacc)
+time_df = data.frame(times)
 
 #-----------------------------------------------------------------
-# Plot unique reactions per network
+# Plot difference in network sizes
 #-----------------------------------------------------------------
 p1 = ggplot(gms_df,aes(x=Num.Growth.Conditions,y=Global.Minus.Seq)) + 
      geom_point(size=3) +
@@ -62,3 +71,17 @@ p1 = ggplot(jacc_df,aes(x=Num.Growth.Conditions,y=Ave.Jaccard.Sim)) +
 print(p1)
 
 ggsave("CE2_jaccard_similarity.tiff",width = 15, height = 9, units = "cm", dpi = 600)
+
+#-----------------------------------------------------------------
+# Plot solution time fold change (Global/Sequential)
+#-----------------------------------------------------------------
+p1 = ggplot(time_df,aes(x=Num.Growth.Conditions,y=Sol.Time.Fold.Change)) + 
+  geom_point(size=3) +
+  geom_errorbar(aes(ymax=upper,ymin=lower)) +
+  theme_bw() +
+  xlab("Num.Growth.Conditions") + 
+  ylab("Solve Time Fold Change (Global/Seq.)") +
+  theme(legend.position="none", text = element_text(size=12))
+print(p1)
+
+ggsave("CE2_solution_time_fold_change.tiff",width = 15, height = 9, units = "cm", dpi = 600)
